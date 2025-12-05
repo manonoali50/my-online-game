248567013b9a3f2007b25b34fb06e9d6f23104e8
manonoali50 <manonoali50@gmail.com>
2025-12-05 20:21:56 +0300

    Refactor socket-client.js for cleaner syntax

--- public/socket-client.js
+++ public/socket-client.js
@@ -1,33 +1,16 @@
 const socket = io();
 
-function createRoom(name, callback){
-  socket.emit('createRoom', { name }, callback);
-}
+function createRoom(name, cb){ socket.emit('createRoom', name, cb); }
+function joinRoom(name, roomId, cb){ socket.emit('joinRoom', name, roomId, cb); }
+function startGame(roomId){ socket.emit('startGame', roomId); }
+function sendAction(roomId, action){ socket.emit('action',{roomId,action}); }
 
-function joinRoom(name, roomId, callback){
-  socket.emit('joinRoom', { name, roomId }, callback);
-}
-
-function startGame(roomId){
-  socket.emit('startGame', roomId);
-}
-
-function sendAction(roomId, action){
-  socket.emit('sendAction', { roomId, action });
-}
-
-// Events
 socket.on('updatePlayers', (players)=>{
-  const ev = new CustomEvent('online_updatePlayers', { detail: players });
-  window.dispatchEvent(ev);
+  window.dispatchEvent(new CustomEvent('online_updatePlayers',{detail:players}));
 });
-
 socket.on('startGame', (players)=>{
-  const ev = new CustomEvent('online_startGame', { detail: players });
-  window.dispatchEvent(ev);
+  window.dispatchEvent(new CustomEvent('online_startGame',{detail:players}));
 });
-
 socket.on('receiveAction', (action)=>{
-  const ev = new CustomEvent('online_receiveAction', { detail: action });
-  window.dispatchEvent(ev);
+  window.dispatchEvent(new CustomEvent('online_receiveAction',{detail:action}));
 });
